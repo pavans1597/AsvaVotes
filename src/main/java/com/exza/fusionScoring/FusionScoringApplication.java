f2d859222055d57bb6f73f2958420acec1d0ee0a
Pavan <pavankumar1597@gmail.com>
2022-11-10 13:08:02 +0530

    initial commit

---
+++ src/main/java/com/exza/fusionScoring/FusionScoringApplication.java
@@ -0,0 +1,13 @@
+package com.exza.fusionScoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionScoringApplication.class, args);
+	}
+
+}
